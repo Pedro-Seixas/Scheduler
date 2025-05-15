5d8415c16500b97af861c11781dec9d750cdf8c2
Pedro <pedrodfs15@gmail.com>
2025-05-15 11:54:34 -0400

    Testing quick sort - Success

--- main.c
+++ main.c
@@ -2,22 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Compare function for qsort
 int comp(const void* a, const void* b){
     Job* job_A = (Job *) a;
     Job* job_B = (Job *) b;
     return job_A->arrival_time - job_B->arrival_time;
 }
+
+// Main function
 int main()
 {    
     // {id, arrival_time, burst_time, time_remaining, priority, timeline}
     Job jobs[3] = {
-        {0, 0, 5, 5, 0, ""},
-        {1, 1, 3, 3, 0, ""},
-        {2, 2, 6, 6, 0, ""}
+        {0, 4, 5, 5, 0, "A"},
+        {1, 1, 3, 3, 0, "B"},
+        {2, 3, 6, 6, 0, "C"}
     };
+    
+    // Defining parameters for qsort function
     int num_jobs = sizeof(jobs)  / sizeof(jobs[0]);
-
+    
+    // Quick Sort
     qsort(jobs, num_jobs, sizeof(Job), comp);
-
+    
+    for(int i = 0; i < 3; i++){
+        printf("%s\n", jobs[i].timeline);
+    }
     return 0;
 }
